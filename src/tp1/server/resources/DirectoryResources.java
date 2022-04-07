9c7757bd0eaef9923569ea0e08e4f977798fe0f0
Blackadder008 <45468352+Blackadder008@users.noreply.github.com>
2022-04-07 16:28:51 +0100

    Made some progress with the server side handling of requests, pay attention to comments.

--- src/tp1/server/resources/DirectoryResources.java
+++ src/tp1/server/resources/DirectoryResources.java
@@ -1,4 +1,69 @@
 package tp1.server.resources;
 
-public class DirectoryResources {
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import tp1.api.FileInfo;
+import tp1.api.User;
+import tp1.api.service.rest.RestDirectory;
+import tp1.clients.RestUsersClient;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class DirectoryResources implements RestDirectory {
+
+    private static Logger Log = Logger.getLogger(DirectoryResources.class.getName());
+
+    private UserResources userResources = new UserResources();
+    private FileResources fileResources = new FileResources();
+
+    public DirectoryResources(){
+    }
+
+    @Override
+    public FileInfo writeFile(String filename, byte[] data, String userId, String password) {
+
+        if(filename == null || data == null || userId == null || password == null){
+            Log.info("Invalid data.");
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        String fileURL = String.format("/%s/%s", userId, filename);
+
+        FileInfo fileInfo = new FileInfo(userId, filename, fileURL, new HashSet<>());
+
+        //What should the token be??
+        String token = "";
+
+        fileResources.writeFile(filename, data, token);
+
+        return fileInfo;
+    }
+
+    @Override
+    public void deleteFile(String filename, String userId, String password) {
+
+    }
+
+    @Override
+    public void shareFile(String filename, String userId, String userIdShare, String password) {
+
+    }
+
+    @Override
+    public void unshareFile(String filename, String userId, String userIdShare, String password) {
+
+    }
+
+    @Override
+    public byte[] getFile(String filename, String userId, String accUserId, String password) {
+        return new byte[0];
+    }
+
+    @Override
+    public List<FileInfo> lsFile(String userId, String password) {
+        return null;
+    }
 }
--- src/tp1/server/resources/FileResources.java
+++ src/tp1/server/resources/FileResources.java
@@ -1,4 +1,92 @@
 package tp1.server.resources;
 
-public class FileResources {
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import tp1.api.FileInfo;
+import tp1.api.service.rest.RestFiles;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class FileResources implements RestFiles {
+
+    private static Logger Log = Logger.getLogger(FileResources.class.getName());
+
+    //Not sure if we should actually write a file or just store the data
+    // to simulate a filesystem
+    //Assuming the second option:
+    private Map<String, byte[]> filesystem = new HashMap<>();
+
+    public FileResources(){
+    }
+
+    @Override
+    public void writeFile(String fileId, byte[] data, String token) {
+
+        //Validity of parameters
+        if(fileId == null || data == null || token == null){
+            Log.info("Invalid data.");
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        //Validate token, how?
+        /* if(!token.isValid()){
+        *   Log.info("Session expired.")
+        *   throw new WebApplicationException(Response.Status.FORBIDDEN);
+        * }
+        * */
+
+        filesystem.put(fileId, data);
+    }
+
+    @Override
+    public void deleteFile(String fileId, String token) {
+
+        if(fileId == null || token == null){
+            Log.info("Invalid data.");
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        //Validate token, how?
+        /* if(!token.isValid()){
+         *   Log.info("Session expired.")
+         *   throw new WebApplicationException(Response.Status.FORBIDDEN);
+         * }
+         * */
+
+        if(filesystem.get(fileId) == null){
+            Log.info("File does not exist.");
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        filesystem.remove(fileId);
+    }
+
+    @Override
+    public byte[] getFile(String fileId, String token) {
+
+        if(fileId == null || token == null){
+            Log.info("Invalid data.");
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+
+        //Validate token, how?
+        /* if(!token.isValid()){
+         *   Log.info("Session expired.")
+         *   throw new WebApplicationException(Response.Status.FORBIDDEN);
+         * }
+         * */
+
+        byte[] file = filesystem.get(fileId);
+
+        if(file == null){
+            Log.info("File does not exist.");
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        return file;
+    }
 }
